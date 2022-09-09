aa3d22344477a8fb1f546ae7c17172fd58698405
Poga Kuofie <mail@pogakuofie.com>
2022-09-09 19:40:01 +0000

    feat: duplicate endpoint removed

--- src/routes/complaint.ts
+++ src/routes/complaint.ts
@@ -5,7 +5,6 @@ import { complaintController } from '../controllers';
 const router = express.Router();
 
 router.get('/complaint', complaintController.getComplaints);
-router.post('/complaint/:id', complaintController.updateComplaintId);
 router.post('/complaint/', complaintController.updateComplaintId);
 
 export default router;
